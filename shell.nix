8c45003f0de3cd176b785f43998e0dbf98246fe9
Travis Athougies <travis@athougies.net>
2019-05-23 12:28:53 -0700

    Do not use local py-intrustd

--- shell.nix
+++ shell.nix
@@ -3,13 +3,13 @@
 let stdenv = pkgs.stdenv;
 
     # TODO figure out how to get node grpc plugin here
-    intrustd-py-srcs = ../../py;
-#      pkgs.fetchFromGitHub {
-#        owner = "intrustd";
-#        repo = "py-intrustd";
-#        rev = "623bf3b701f8381fad56c082e38b211f9d782474";
-#        sha256 = "042m5z1pcb1v6jz8qwqw7366md2gyrpblzpbs64iamfcwdx57sc0";
-#      };
+    intrustd-py-srcs =
+      pkgs.fetchFromGitHub {
+        owner = "intrustd";
+        repo = "py-intrustd";
+        rev = "623bf3b701f8381fad56c082e38b211f9d782474";
+        sha256 = "042m5z1pcb1v6jz8qwqw7366md2gyrpblzpbs64iamfcwdx57sc0";
+      };
 
     intrustd-py = pkgs.callPackage intrustd-py-srcs { };
 
