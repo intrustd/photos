7dceae057f8e11ab3d158dfcac8c16bb99ad12da
Travis Athougies <travis@athougies.net>
2019-05-23 09:57:26 -0700

    Make sure to list packages explicitly

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="intrustd-photos",
     version="0.1.0",
     description="Intrustd Photo App",
-    packages=find_packages(),
+    packages=['intrustd.photo'],
     install_requires=["Flask>=0.2", "Pillow" ],
     entry_points={
         'console_scripts': [ 'photos=intrustd.photo:main', 'photo-perms=intrustd.photo.perms:verify' ]
